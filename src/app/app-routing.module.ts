113bc08c59c51ebe33afa81ab36db40113d4ed7f
unknown <menah.vye@protonmail.com>
2023-02-23 23:16:09 +0100

    'first-com'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CryptoComponent } from './crypto/crypto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: CryptoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
